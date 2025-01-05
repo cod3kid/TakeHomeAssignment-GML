d5193e60538841d23a024760bcb39b15e3550807
cod3kid <djsufail@gmail.com>
2025-01-05 22:32:07 +0530

    Open a WebSocket communication

--- ping-pong-fe/src/scenes/Game.js
+++ ping-pong-fe/src/scenes/Game.js
@@ -4,23 +4,45 @@ export class Game extends Scene {
   constructor() {
     super("Game");
 
-    this.playerAPaddle;
-    this.playerBPaddle;
+    this.playerPaddle;
+    this.opponentPaddle;
+    this.socket;
+    this.ball;
     this.paddleSpeed = 10;
   }
 
   create() {
-    this.createPaddles();
+    this.createWebSocketCommunication();
+    this.createPaddle();
     this.createBall();
   }
 
-  createPaddles() {
-    this.playerAPaddle = this.physics.add
+  createWebSocketCommunication() {
+    this.socket = new WebSocket("ws://localhost:8000/ws");
+
+    this.socket.onmessage = (event) => {
+      const data = JSON.parse(event.data);
+      console.log(data);
+
+      // Update opponent paddle position
+      if (data.opponentY !== undefined) {
+        this.opponentPaddle.y = data.opponentY;
+      }
+
+      // Update ball position
+      if (data.ballX !== undefined && data.ballY !== undefined) {
+        ball.setPosition(data.ballX, data.ballY);
+      }
+    };
+  }
+
+  createPaddle() {
+    this.playerPaddle = this.physics.add
       .image(20, this.scale.canvas.height / 2, "paddle")
       .setTintFill(0xff0000)
       .setScale(0.0625, 0.25);
 
-    this.playerBPaddle = this.physics.add
+    this.opponentPaddle = this.physics.add
       .image(
         this.scale.canvas.width - 20,
         this.scale.canvas.height / 2,
@@ -29,22 +51,24 @@ export class Game extends Scene {
       .setTintFill(0xff0000)
       .setScale(0.0625, 0.25);
 
-    this.createPaddlesListener();
+    this.createPaddleListener();
   }
 
-  createPaddlesListener() {
+  createPaddleListener() {
     this.input.keyboard.on("keydown", (event) => {
       if (event.key === "ArrowUp") {
-        this.playerAPaddle.setY(this.playerAPaddle.y - this.paddleSpeed);
+        this.playerPaddle.setY(this.playerPaddle.y - this.paddleSpeed);
+        this.socket.send(JSON.stringify({ playerA: this.playerPaddle.y }));
       } else if (event.key === "ArrowDown") {
-        this.playerAPaddle.setY(this.playerAPaddle.y + this.paddleSpeed);
+        this.playerPaddle.setY(this.playerPaddle.y + this.paddleSpeed);
+        this.socket.send(JSON.stringify({ playerY: this.playerPaddle.y }));
       }
     });
   }
 
   createBall() {
     this.ball = this.physics.add
-      .image(100, 100, "ball")
+      .image(this.cameras.main.width / 2, this.cameras.main.height / 2, "ball")
       .setTintFill(0xffffff)
       .setScale(0.05);
   }
