85b45c48a60f51af97d1fad00ec0826beb9a5b23
cod3kid <djsufail@gmail.com>
2025-01-05 22:08:52 +0530

    Create Paddles for both players

--- ping-pong-fe/src/scenes/Game.js
+++ ping-pong-fe/src/scenes/Game.js
@@ -3,7 +3,28 @@ import { Scene } from "phaser";
 export class Game extends Scene {
   constructor() {
     super("Game");
+
+    this.playerAPaddle;
+    this.playerBPaddle;
   }
 
-  create() {}
+  create() {
+    this.createPaddles();
+  }
+
+  createPaddles() {
+    this.playerAPaddle = this.add
+      .image(20, this.scale.canvas.height / 2, "paddle")
+      .setTintFill(0xff0000)
+      .setScale(0.0625, 0.25);
+
+    this.playerBPaddle = this.add
+      .image(
+        this.scale.canvas.width - 20,
+        this.scale.canvas.height / 2,
+        "paddle"
+      )
+      .setTintFill(0xff0000)
+      .setScale(0.0625, 0.25);
+  }
 }
